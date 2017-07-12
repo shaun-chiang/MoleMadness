574c4594e31852f7451ae1bb79cae304a0285e30
Shaun Chiang <redbeansodapop@hotmail.com>
2017-07-12 17:59:12 +0800

    Added basic login-register
    Also imported gamesparks, facebook (for future facebook login). Moved scenes to scene folder.

---
+++ MoleMadness/Assets/GameSparksManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSparksManager : MonoBehaviour
+{
+
+    private static GameSparksManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ MoleMadness/Assets/RegisterPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+
+public class RegisterPlayer : MonoBehaviour
+{
+
+    public Text displayNameInput, userNameInput, passwordInput, notificationText;
+    public void RegisterPlayerBttn()
+    {
+        Debug.Log("Registering..");
+
+        new RegistrationRequest()
+          .SetDisplayName(displayNameInput.text)
+          .SetPassword(passwordInput.text)
+          .SetUserName(displayNameInput.text)
+          .Send((response) =>
+          {
+              if (!response.HasErrors)
+              {
+                  Debug.Log("Player Registered");
+                  notificationText.text = "Player Registered";
+              }
+              else
+              {
+                  Debug.Log("Error Registering Player");
+                  notificationText.text = "Error Registering Player";
+              }
+          }
+        );
+    }
+
+    public void AuthorizePlayerBttn()
+    {
+        Debug.Log("Authorizing Player...");
+        new AuthenticationRequest()
+            .SetUserName(userNameInput.text)
+            .SetPassword(passwordInput.text)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log("Player Authenticated...");
+                    notificationText.text = "Player Authenticated";
+                    PlayerPrefs.SetString("PlayerName", userNameInput.text);
+                    SceneManager.LoadScene(1);
+
+                }
+                else
+                {
+                    Debug.Log("Error Authenticating Player...");
+                    notificationText.text = "Authentication Error";
+
+                }
+            });
+    }
+}

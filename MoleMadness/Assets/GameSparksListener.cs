f133d84cd0605629ac588ce0c786b8795056976f
Lin Junsheng <junsheng_lin@mymail.sutd.edu.sg>
2017-07-27 15:07:20 +0800

    Fixed player1 bug

--- MoleMadness/Assets/GameSparksListener.cs
+++ MoleMadness/Assets/GameSparksListener.cs
@@ -20,8 +20,8 @@ public class GameSparksListener : MonoBehaviour {
         PlayerPrefs.SetString("GameSeed",seed); //use this as seed for match
         string myId = PlayerPrefs.GetString("playerId").Replace("\"","");
         Debug.Log(string.Format("myID: {0}", myId));
-        string player1Id = jsonmessage["challenge"]["scriptData"]["player1"].ToString();
-        Debug.Log(player1Id);
+        string player1Id = jsonmessage["challenge"]["scriptData"]["player1"].ToString().Replace("\"", "");
+        Debug.Log(string.Format("player1Id: {0}", player1Id));
         if (myId == player1Id)
         {
             PlayerPrefs.SetInt("player1", 1);

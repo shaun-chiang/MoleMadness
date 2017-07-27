a013f820dee58675f55aae80c9d0ea8857b73630
purple-kisa <hazellau.rcy@gmail.com>
2017-07-27 15:38:46 +0800

    renamed files to fit server names

--- MoleMadness/Assets/_Scripts/wardrobe_scripts/wardrobeManager.cs
+++ MoleMadness/Assets/_Scripts/wardrobe_scripts/wardrobeManager.cs
@@ -24,6 +24,8 @@ public class wardrobeManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
+        initializeEquipped(); 
+
         initializeLists("hats", hats_list);
         initializeLists("clothes", clothes_list);
         initializeLists("baby", baby_list);
@@ -62,5 +64,35 @@ public class wardrobeManager : MonoBehaviour {
                 }
             }
         }
+    }
+
+    void initializeEquipped()
+    {
+        string equipped = PlayerPrefs.GetString("equipped");
+        //Debug.Log(equippedList2);
+
+        string[] equippedList;
+
+        equipped = equipped.Substring(1, equipped.Length - 2);
+        equippedList = equipped.Split(' ');
+        for (int i = 0; i < equippedList.Length; i++)
+        {
+            equippedList[i] += "_ON";
+        }
+
+        GameObject hat = Instantiate(Resources.Load("hats/" + equippedList[0], typeof(GameObject))) as GameObject;
+        hat.transform.position = new Vector3(-220, -30, 0);
+        hat.transform.localRotation = Quaternion.identity;
+        hat.tag = "hat";
+
+        GameObject weapon = Instantiate(Resources.Load("weapons/" + equippedList[1], typeof(GameObject))) as GameObject;
+        weapon.transform.position = new Vector3(-220, -30, 0);
+        weapon.transform.localRotation = Quaternion.identity;
+        weapon.tag = "weapon";
+
+        GameObject baby = Instantiate(Resources.Load("baby/" + equippedList[2], typeof(GameObject))) as GameObject;
+        baby.transform.position = new Vector3(-80, -45, 0);
+        baby.transform.localRotation = Quaternion.identity;
+        baby.tag = "baby";
     }
 }

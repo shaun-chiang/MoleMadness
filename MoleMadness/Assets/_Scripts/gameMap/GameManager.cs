98f639e74faa7cfa0be0e7a037188ac3c19c5658
Lin Junsheng <junsheng_lin@mymail.sutd.edu.sg>
2017-07-31 15:19:56 +0800

    Move check logic to be tested

--- MoleMadness/Assets/_Scripts/gameMap/GameManager.cs
+++ MoleMadness/Assets/_Scripts/gameMap/GameManager.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 using GameSparks.Api.Responses;
 using GameSparks.Api.Requests;
 
-public class GameManager{
+public class GameManager
+{
 
     public enum GameState { SPAWNINGMOTHER, SPAWNINGBABY, PLAYERTURN, OPPONENTTURN, RESPAWNBABY };
     public enum Characters { MOTHER, BABY }
+    public enum MoveResult { NOTHING, HITBABY }
 
     public GameState currentGameState;
     public static GameManager gameManagerinstance;
     public static MapManager mapManagerinstance;
     public bool player1;
+    public int movesLeft;
+    public bool myTurn;
+    public static int myBabyHealth;
+    public static int oppBabyHealth;
+    private MoveResult moveResultHolder;
 
     public GameManager(GameState gameState)
     {
         currentGameState = gameState;
-        // for testing, to be init from gamespark
-        player1 = true;
         if (gameManagerinstance == null)
         {
             gameManagerinstance = this;
@@ -35,27 +40,35 @@ public class GameManager{
         return PlayerPrefs.GetString("GameSeed");
     }
 
-    public static void getPlayerId()
+    public static void initGame(JSONObject jsonmessage)
     {
-        new AccountDetailsRequest().Send((response) =>
+        // init new game
+        Debug.Log(jsonmessage);
+        string seed = jsonmessage["challenge"]["challengeId"].ToString().Replace("\"", "");
+        Debug.Log(seed);
+        PlayerPrefs.SetString("GameSeed", seed); //use this as seed for match
+        string myId = PlayerPrefs.GetString("playerId").Replace("\"", "");
+        Debug.Log(string.Format("myID: {0}", myId));
+        string player1Id = jsonmessage["challenge"]["scriptData"]["player1"].ToString().Replace("\"", "");
+        Debug.Log(string.Format("player1Id: {0}", player1Id));
+        if (myId == player1Id)
         {
-            Debug.Log("Requesting Account Details...");
-            if (!response.HasErrors)
-            {
-                Debug.Log("Got Player Details");
-                Debug.Log(response.JSONString);
-            }
-            else
-            {
-                Debug.Log("Error Receiving Account Details");
-            }
-        });
+            PlayerPrefs.SetInt("player1", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("player1", 0);
+        }
+
+        // init Baby Health
+        myBabyHealth = 3;
+        oppBabyHealth = 3;
     }
 
     public static void initPosition(int MotherX, int MotherZ, int BabyX, int BabyZ)
     {
         string cid = getChallengeId();
-        Debug.Log(string.Format("Init mother at {0},{1} and baby at {2},{3} using challengeId {4}",MotherX,MotherZ,BabyX,BabyZ,cid));
+        Debug.Log(string.Format("Init mother at {0},{1} and baby at {2},{3} using challengeId {4}", MotherX, MotherZ, BabyX, BabyZ, cid));
         new LogChallengeEventRequest().SetEventKey("action_SETPOS")
            .SetEventAttribute("challengeInstanceId", cid)
            .SetEventAttribute("babyX", BabyX)
@@ -75,7 +88,37 @@ public class GameManager{
                    Debug.Log(response.JSONString);
                }
            });
+    }
 
+    public static void sendMove(int x, int z)
+    {
+        // send movement to server to check for feedback
+        new LogChallengeEventRequest().SetEventKey("action_MOVECHECK")
+            .SetEventAttribute("challengeInstanceId", getChallengeId())
+            .SetEventAttribute("posX", x)
+            .SetEventAttribute("posY", z)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                { 
+                    JSONObject jsonmessage = new JSONObject((JSONObject)response.JSONData);
+                    Debug.Log(jsonmessage["scriptData"]);
+                    if (jsonmessage["scriptData"]["Result"].ToString() == "Hit Baby")
+                    {
+                        Debug.Log("Opponent baby mole hit");
+                        oppBabyHealth -= 1;
+                    }
+                    else
+                    {
+                        Debug.Log("Nothing particular happen");
+                    }
+                    mapManagerinstance.movePlayer(x, z);
+                }
+                else
+                {
+                    Debug.Log("Unsuccessful move check");
+                }
+            });
 
     }
 

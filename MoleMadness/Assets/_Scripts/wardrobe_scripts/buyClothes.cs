f1f080c816203d8fa2a514841fa2615d47bc8a2d
purple-kisa <hazellau.rcy@gmail.com>
2017-08-07 10:24:39 +0800

    added some image prefabs and finished the popup for buying items left with server logic for buying items

--- MoleMadness/Assets/_Scripts/wardrobe_scripts/buyClothes.cs
+++ MoleMadness/Assets/_Scripts/wardrobe_scripts/buyClothes.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameSparks.Api.Requests;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class buyClothes : MonoBehaviour {
 
-    public GameObject popup; 
+    public GameObject popup;
+
+    public Font font;
 
 	// Use this for initialization
 	void Start () {
@@ -18,14 +21,35 @@ public class buyClothes : MonoBehaviour {
 		
 	}
 
-    public void buy(GameObject accessory)
+    public void buy(GameObject sprite)
     {
+        Debug.Log(this.name);
         Debug.Log("buy");
         popup = Instantiate(Resources.Load("popup", typeof(GameObject)) as GameObject);
         popup.transform.SetParent(GameObject.Find("Canvas").transform);
-        popup.transform.position = new Vector3(150, 18, 0);
+        popup.transform.position = new Vector3(150, 10, 0);
         popup.transform.localRotation = Quaternion.identity;
         popup.SetActive(true);
+
+        //0,63,0 (text) 
+        GameObject nameGO = new GameObject("name");
+        Text name = nameGO.AddComponent<Text>();
+        RectTransform trans = name.GetComponent(typeof(RectTransform)) as RectTransform;
+        trans.sizeDelta = new Vector2(200, 100);
+        name.text = this.name.Substring(0, this.name.Length - 12).Replace('_', ' ');
+        name.transform.SetParent(popup.transform);
+        name.color = Color.black;
+        name.font = font; 
+        name.transform.position = new Vector3(150, 65, 0);
+        name.transform.localRotation = Quaternion.identity;
+
+        GameObject img = Instantiate(sprite);
+        img.transform.SetParent(popup.transform);
+        img.transform.position = new Vector3(100, 48, 0);
+        img.transform.localRotation = Quaternion.identity;
+        
+        
+        
 
     }
 

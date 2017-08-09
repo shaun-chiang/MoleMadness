26356ae7b9c82c46f8c9850d12e0d94a6d18cbec
Shaun Chiang <redbeansodapop@hotmail.com>
2017-08-10 03:20:13 +0800

    Added some background music, matchmaking loading animation

---
+++ MoleMadness/Assets/_Scripts/AnimationLoadingScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationLoadingScript : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    public float framesPerSecond;
+
+    private SpriteRenderer spriteRenderer;
+
+    bool animate;
+    float time_start;
+    float animation_time;
+
+    // Use this for initialization
+    void Start()
+    {
+        animate = false;
+        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+        animation_time = sprites.Length / framesPerSecond;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!animate)
+        {
+            print("Reached");
+            animate = true;
+            time_start = Time.timeSinceLevelLoad;
+        }
+
+        if (animate)
+        {
+            int index = (int)((Time.timeSinceLevelLoad - time_start) * framesPerSecond);
+            index = index % sprites.Length;
+            spriteRenderer.sprite = sprites[index];
+            if (Time.timeSinceLevelLoad >= time_start + animation_time)
+            {
+                animate = false;
+                spriteRenderer.sprite = null;
+            }
+        }
+    }
+}

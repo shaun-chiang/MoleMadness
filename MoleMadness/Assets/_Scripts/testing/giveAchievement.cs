94860d05e29aacf6e3f54ff92c682620cd1cf466
Shaun Chiang <redbeansodapop@hotmail.com>
2017-08-09 15:50:44 +0800

    Added testing button for first win achievement

---
+++ MoleMadness/Assets/_Scripts/testing/giveAchievement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api.Requests;
+public class giveAchievement : MonoBehaviour {
+	// Use this for initialization
+	public void achieveButton()
+    {
+        new LogEventRequest().SetEventKey("AWARD_ACHIEVEMENT")
+            .SetEventAttribute("ACH_NAME", "FIRST_WIN") // change string to whatever achievement shortcode you need
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log("Achievement awarded");
+                }
+                else
+                {
+                    Debug.Log("Error with Achievement awarding");
+                }
+            });
+    }
+
+}
+

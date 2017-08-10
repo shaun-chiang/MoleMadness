a442186bf33da3da56e30b69802b4b8afab1250e
Shaun Chiang <redbeansodapop@hotmail.com>
2017-08-10 10:13:02 +0800

    Changed postmatch scenes to look better :D

--- MoleMadness/Assets/RankLoseChange.cs
+++ MoleMadness/Assets/RankLoseChange.cs
@@ -7,6 +7,11 @@ public class RankLoseChange : MonoBehaviour {
     private int rank;
     private int stars;
 
+    public Sprite star;
+
+    public GameObject star1, star2, star3, star4, star5;
+    public GameObject star6, star7, star8, star9, star10;
+
     public Text oldRankStarText, newRankStarText;
     // Use this for initialization
     void Start()
@@ -15,7 +20,36 @@ public class RankLoseChange : MonoBehaviour {
         stars = int.Parse(PlayerPrefs.GetString("stars"));
         Debug.Log("Old Rank: " + rank);
         Debug.Log("Old stars: " + stars);
-        oldRankStarText.text = "Rank " + rank.ToString() + " Stars " + stars.ToString();
+        oldRankStarText.text = rank.ToString();
+
+
+        List<GameObject> starList = new List<GameObject>();
+        starList.Add(star1);
+        starList.Add(star2);
+        starList.Add(star3);
+        starList.Add(star4);
+        starList.Add(star5);
+
+
+        if (rank >= 11)
+        {
+            star5.SetActive(false);
+            if (rank >= 16)
+            {
+                star4.SetActive(false);
+                if (rank >= 20)
+                {
+                    star3.SetActive(false);
+                }
+            }
+        }
+
+        for (int i = 0; i < stars; i++)
+        {
+            Image img = starList[i].GetComponentInChildren<Image>();
+            img.sprite = star;
+        }
+
         if (stars>0)
         {
             stars--;
@@ -42,6 +76,33 @@ public class RankLoseChange : MonoBehaviour {
         Debug.Log("New Stars: " + stars);
         PlayerPrefs.SetString("rank", rank.ToString());
         PlayerPrefs.SetString("stars", stars.ToString());
-        newRankStarText.text = "Rank " + rank.ToString() + " Stars " + stars.ToString();
+        newRankStarText.text = rank.ToString();
+
+        List<GameObject> starList2 = new List<GameObject>();
+        starList2.Add(star6);
+        starList2.Add(star7);
+        starList2.Add(star8);
+        starList2.Add(star9);
+        starList2.Add(star10);
+
+
+        if (rank >= 11)
+        {
+            star10.SetActive(false);
+            if (rank >= 16)
+            {
+                star9.SetActive(false);
+                if (rank >= 20)
+                {
+                    star8.SetActive(false);
+                }
+            }
+        }
+
+        for (int i = 0; i < stars; i++)
+        {
+            Image img = starList2[i].GetComponentInChildren<Image>();
+            img.sprite = star;
+        }
     }
 }
